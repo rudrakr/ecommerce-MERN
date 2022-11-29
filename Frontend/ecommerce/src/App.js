1cc94d3cc6a3876d86eb543bf97b459f6e973d82
Rudra Banerjee <rudrabanerjee15@gmail.com>
2022-11-29 13:25:46 +0000

    file changed

---
+++ Frontend/ecommerce/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Amazonia
+        <main>Product list</main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
